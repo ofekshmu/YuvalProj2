45eeef5f9886607a1fffd6816540b46f0d6dfbfd
Ofek Shmuel <ofek.shmuel1@gmail.com>
2022-05-04 14:48:46 +0300

    created vec X

--- Main.R
+++ Main.R
@@ -1,5 +1,33 @@
 library(igraph)
 
+#' Initialize weights of graph with the rexp function (exp(teta))
+#' as stated in the task info
+#' teta = 0.02
+InitWeights <- function(g){
+  n = length(E(g))
+  E(g)$weight <- rexp(n,0.02)
+  return(g)
+}
+
+#' generates vector X as explained.
+#' The vector will indicate functionality of each edge after 10 days.
+Get_X_vector <- function(g){
+  edges <- E(g)$weight
+  print(edges)
+  x_vec <- seq(from=0,to=0,length=10)
+  for( i in 1:length(edges)){
+    if (edges[i] >= 10){
+      x_vec[i] = 1
+    } 
+    else{
+      x_vec[i] = 0
+    }
+  }
+  return(x_vec)
+}
+
+
+
 g <- graph_from_literal( A -+ E , B -+ F , C -+ F ,
                          A -+ D , B -+ C , C -+ G ,
                          A -+ C , E -+ I , C -+ D ,
@@ -9,9 +37,12 @@ g <- graph_from_literal( A -+ E , B -+ F , C -+ F ,
                          D -+ I , G -+ H , I -+ J ,
                          D -+ E)
                          
-all_simple_paths(g,
+paths <- all_simple_paths(g,
                  'A',
                  'J', mode = c("out"))
                  
 
+g <- InitWeights(g)
+x <- Get_X_vector(g)
+print(x)
 
